3fd0c6920784cef9ebb1a12c83f7a93d84ca29d9
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-05-12 07:14:59 -0300

    [NG] LAZY_LOADING_SERVICES - feat: lazy loading the TasksService for the users.routes route group

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -2,24 +2,31 @@ import { Routes } from '@angular/router';
 
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
 import { resolveUserTasks } from '../tasks/task/task.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: () => import('../tasks/tasks.component').then(module => module.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        loadComponent: () => import('../tasks/tasks.component').then(module => module.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
   },
 ];
